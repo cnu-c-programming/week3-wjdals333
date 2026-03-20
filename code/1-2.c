fec6cdda0b344ee53cdeaa407891b805601db941
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-03-20 09:10:09 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void print() {
+    printf("Welcome to Computer Programming 03 Lab!\n");
+}
+
+int square(int n) {
+    return n * n;
+}
+int main(void)
+
+{
+    print();
+
+    int num = 5;
+    int result = square(num);
+    printf("%d %d\n", num, result);
+    
+    return 0;
+}
