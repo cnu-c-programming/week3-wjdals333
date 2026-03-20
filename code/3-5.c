b94410412936c7a257fcd6dec0a3b8970431b339
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-03-20 10:24:04 +0900

    Update 3-5.c

---
+++ code/3-5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_sum(char type, int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+
+
+    if (type == 'S')
+    {
+        for (int i = 0; i < count; i++) {
+            printf("%s ", va_arg(ap, char *));
+        }
+        printf("\n");
+    }
+
+    else if (type == 'C')
+    {
+        for (int i = 0; i < count; i++) {
+            printf("%c", va_arg(ap, int));
+        }
+        printf("\n");
+    }
+    else if (type == 'D')
+    {   
+        int total = 0;
+        for (int i = 0; i < count; i++) {
+            total += va_arg(ap, int);
+        }
+        printf("%d\n", total);
+    }
+}
+
+int main() {
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
+
+    return 0;
+}
